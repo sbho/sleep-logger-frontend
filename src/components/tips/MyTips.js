dbdfb18a56b47b0545fa71c96ced9a66b7f69779
Ho Si Binh <e0411175@u.nus.edu>
2020-07-21 21:47:15 +0800

    Add a message to user when there is no saved tip.

--- src/components/tips/MyTips.js
+++ src/components/tips/MyTips.js
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from "react";
+import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
 import Grid from "@material-ui/core/Grid";
 import DeletableTipBox from "./DeletableTipBox";
 import LoggedInNavBar from "../nav/LoggedInNavBar";
+import { Link } from "react-router-dom";
+import Button from "@material-ui/core/Button";
 
 export default function MyTips(props) {
   const [tips, setTips] = useState([]);
+  const [hasNoTip, setHasNoTip] = useState(false);
 
   useEffect(() => {
     const BASE_URL = "https://sleep-logger-dev.herokuapp.com";
@@ -23,6 +27,9 @@ export default function MyTips(props) {
       .then((res) => res.json())
       .then((data) => {
         setTips(data);
+        if (data.length === 0) {
+          setHasNoTip(true);
+        }
       });
   });
 
@@ -38,11 +45,36 @@ export default function MyTips(props) {
   return (
     <div>
       <LoggedInNavBar history={props.history} />
-      <Box p={"5%"}>
+      {hasNoTip && (
         <Grid container spacing={2}>
-          {tipBoxes}
+          <Grid container item xs={4}>
+            {/*empty*/}
+          </Grid>
+          <Grid container item xs={4}>
+            <Box p={"5%"}>
+              <Typography variant={"h5"}>
+                You have not saved any tip. Time to save some!
+              </Typography>
+              <Box p={"2%"}>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  component={Link}
+                  to="/diary"
+                >
+                  Take me to my diary
+                </Button>
+              </Box>
+            </Box>
+          </Grid>
+          <Grid container item xs={4}>
+            {/*empty*/}
+          </Grid>
         </Grid>
-      </Box>
+      )}
+      <Grid container spacing={2}>
+        <Box p={"5%"}>{tipBoxes}</Box>
+      </Grid>
     </div>
   );
 }
