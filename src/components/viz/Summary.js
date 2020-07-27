5df29d9e22e0b71b275868bfcba6922fdfcd9542
Ho Si Binh <e0411175@u.nus.edu>
2020-07-27 21:33:24 +0800

    Fix graph labelling ordering

--- src/components/viz/Summary.js
+++ src/components/viz/Summary.js
@@ -43,7 +43,7 @@ export default class Summary extends React.Component {
         y: k.count,
       })
     );
-    return XYData;
+    return XYData.sort((a, b) => a.x >= b.x);
   };
 
   toXYData = (data, fieldName) => {
@@ -62,7 +62,7 @@ export default class Summary extends React.Component {
     data[fieldName].forEach((k) =>
       XYData.push({ x: k[fieldName] ? "Yes" : "No", y: k.count })
     );
-    return XYData;
+    return XYData.sort((a, b) => (a === "Yes" ? -1 : 1));
   };
 
   componentDidMount() {
